c21dde5110c95f684d3dd8d10901c8eee47cb404
Sahachok.J <sahachok.j@kbtg.tech>
2022-04-29 11:13:08 +0700

    Property binding and event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'hello123';
   name = '';
+  nickname = '';
   doSth(event: Event){
     let val = (event.target as HTMLInputElement).value;
     this.name = val;
+  }
+
+  doSth2(name: string){
+    this.name = name;
+  }
+
+  doNickname(nickname : string){
+    console.log(nickname);
+    this.nickname = nickname;
   }
 }
